b4aae836483a8b44e71a64dc255942e83dff090d
Alex Vorobin <volnyc1@aol.com>
2023-08-29 01:13:17 -0400

    first commit for lesson08_adv

--- script.js
+++ script.js
@@ -1,17 +1,122 @@
 'use strict';
 
-const week = ['понедельник','вторник','среда','четверг','пятница','суббота','воскресенье'];
-const weekend = ['суббота','воскресенье'];
+const appData = {
+    title: '',
+    screens: [], 
+    screenPrice: 0, 
+    adaptive: true,
+    rollback: 10,
+    allServicePrices: 0,
+    fullPrice: 0,
+    servicePercentPrice: 0,
+    additionalServices: {},
 
-// Вывести на экран все дни недели
-console.log(week);
+    asking: function () {
+        do {          
+            appData.title = prompt("Как называется ваш проект?");
+        } while (!appData.isProperString(appData.title));
+              
+        for (let i = 0; i < 2; i++) {
+            let name = '';
+            let x = 0;
 
-// Каждый из них с новой строчки
-week.forEach((item) => console.log(item));
+            do {          
+                name = prompt("Какие типы экранов нужно разработать?");
+            } while (!appData.isProperString(name));           
+           
+            do {
+                x = prompt("Сколько будет стоить данная работа?");
+            } while (!appData.isNumber(x));
+            appData.screens.push({id: i, name: name, price: parseFloat(x)});
+        }
 
-// Выходные дни - курсивом
-week.forEach((item) => weekend.includes(item) ? console.log(`%c${item}`, 'font-style: italic;') : console.log(item));
+        for (let i = 0; i < 2; i++) {
+            let name = '';
+            let x = 0;
 
-// Текущий день - жирным шрифтом(использовать объект даты)
-let today = new Date().toLocaleDateString('ru-RU',{weekday: 'long'}, {timeZone: "Europe/Moscow"});
-week.forEach((item) => (item === today) ? console.log(`%c${item}`, 'font-weight: bold;') : console.log(item));
+            do {          
+                name = prompt("Какой дополнительный тип услуги нужен?");
+            } while (!appData.isProperString(name));            
+
+            do {
+                x = prompt("Сколько это будет стоить?");
+            } while (!appData.isNumber(x));
+
+            // done by simply concat name to id. Dash is needed to make it unique, otherwise for example:
+            // "name1" + 1  is equal to "name" + 11
+            // could also be done like Object of Objects {{},{}....} or Object of keys with Array {1: [name, key], ...} 
+            appData.additionalServices[(name + "-" + i)] = parseFloat(x);                          
+        }
+            
+        appData.adaptive = (prompt("Нужен ли адаптив на сайте?") == "true" ? true: false);       
+    },
+
+    addPrices: function() {
+        appData.screenPrice = appData.screens.reduce((total, num) => {return total + num.price}, 0);
+
+        for (let key in appData.additionalServices) {
+            appData.allServicePrices += appData.additionalServices[key];
+        }
+    },
+
+    // Answer prompt string is also checked for null, empty and all spaces as invalid
+    isProperString: function(prompt_string) {
+        if (prompt_string == null) {
+            return false;
+        } else if (prompt_string == "") {
+            return false;
+        } else if (prompt_string.trim().length == 0) {
+            return false;
+        } else {            
+            return !(!isNaN(parseFloat(prompt_string)) && isFinite(prompt_string));
+        }
+    },
+
+    isNumber: function (num) {    
+        return !isNaN(parseFloat(num)) && isFinite(num);
+    },
+
+    getFullPrice: function () {
+        appData.fullPrice = (appData.screenPrice + appData.allServicePrices);
+    },
+    
+    getTitle: function () {
+        appData.title = ((appData.title.trim())[0].toUpperCase() + (appData.title.trim().slice(1).toLowerCase()));
+    },
+    
+    getServicePercentPrices: function () {
+        appData.servicePercentPrice = (Math.ceil(appData.fullPrice - (appData.fullPrice * (appData.rollback/100))));
+    },
+    
+    getRollbackMessage: function (price) {
+        if (price >= 30000) {
+            return ("Даем скидку в 10%");
+        } else if (price >=15000 && price < 30000) {
+            return ("Даем скидку в 5%");
+        } else if (price >= 0) {
+            return ("Скидка не предусмотрена");
+        } else {
+            return ("Что то пошло не так");
+        }
+    },
+
+    logger: function() {
+        console.log(appData.allServicePrices);
+        console.log(appData.screenPrice);
+        console.log(appData.screens);
+        console.log(appData.getRollbackMessage(appData.fullPrice));
+        console.log(appData.servicePercentPrice);
+    },
+
+    start: function() {
+        appData.asking();
+        appData.addPrices();
+        appData.getFullPrice();
+        appData.getServicePercentPrices();
+        appData.getTitle();
+        appData.logger();
+    }
+
+}
+
+appData.start();
